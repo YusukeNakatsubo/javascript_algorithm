55773821132914a53767ccc7306aa69ce2265032
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-09-08 13:37:35 +0900

    Edit file 'factorialRecursive'

--- beginner/factorial/test/factorialRecursive.test.js
+++ beginner/factorial/test/factorialRecursive.test.js
@@ -1,12 +1,12 @@
 const assert = require('assert');
-const factorial = require('../factorialRecursive')
+const factorialRecursive = require('../factorialRecursive')
 
-describe('factorial', () => {
+describe('factorialRecursive', () => {
   it('should calculate factorial', () => {
-    assert.equal(factorial(0), 1)
-    assert.equal(factorial(1), 1)
-    assert.equal(factorial(5), 120)
-    assert.equal(factorial(8), 40320)
-    assert.equal(factorial(10), 3628800)
+    assert.equal(factorialRecursive(0), 1)
+    assert.equal(factorialRecursive(1), 1)
+    assert.equal(factorialRecursive(5), 120)
+    assert.equal(factorialRecursive(8), 40320)
+    assert.equal(factorialRecursive(10), 3628800)
   });
 });
