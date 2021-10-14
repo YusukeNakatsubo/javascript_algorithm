aae916a193283d214725a14e4f34ad4178359ec8
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-10-14 13:04:28 +0900

    Edit files 'binarySearch'.

--- beginner/binarySearch/binarySearch.js
+++ beginner/binarySearch/binarySearch.js
@@ -1,27 +1,28 @@
 /**
  * @param {*[]} array
+ * @param {function(a)}
  * @return {number} result
  */
-const binarySearch = (array) => {
+const binarySearch = (array, number) => {
   let result      = -1,
-      seekElement = 2,
-      leftIndex   = 0,
-      rightIndex  = array.length - 1,
+      seekElement = number,
+      startIndex   = 0,
+      endIndex  = array.length - 1,
       middleIndex;
 
   if (array.length < 1) { return result; }
-  while (leftIndex <= rightIndex ) {
-    middleIndex = Math.floor((rightIndex + leftIndex) / 2);
+  while (startIndex <= endIndex ) {
+    middleIndex = Math.floor((endIndex + startIndex) / 2);
     if(array[middleIndex] === seekElement) {
       result = middleIndex;
       break;
     } else if (array[middleIndex] < seekElement) {
-      leftIndex = middleIndex + 1;
+      startIndex = middleIndex + 1;
     } else if (array[middleIndex] > seekElement) {
-      rightIndex = middleIndex - 1;
+      endIndex = middleIndex - 1;
     }
   }
   return result;
 }
 let array = [1, 3, 10, 2, 8].sort((a, b) => a - b);
-console.log(binarySearch(array));
+console.log(binarySearch(array, 2));
