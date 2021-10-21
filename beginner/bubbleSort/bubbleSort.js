1b61389986348500d82bcb1ed901b5dccabe2946
YusukeNakatsubo <yusuke.nakatsubo@rockinon.co.jp>
2021-10-21 11:24:34 +0900

    Edit files 'Bubble Sort'.

--- beginner/bubbleSort/bubbleSort.js
+++ beginner/bubbleSort/bubbleSort.js
@@ -1,14 +1,14 @@
 /**
  * @param {*[]} array
- * @return {*[]} result
+ * @return {number[]} result
  */
 const bubbleSort = (array) => {
+  if (array.length === 0) { return array; }
+
   for (let i = 0; i < array.length; i += 1) {
     for (let j = 0; j < array.length; j += 1) {
       if (array[j] > array[j+1]) {
-        let tmp = array[j]
-        array[j] = array[j+1];
-        array[j+1] = tmp;
+        [array[j], array[j+1]] = [array[j+1], array[j]]
       }
     }
   }
